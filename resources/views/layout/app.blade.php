560db48348258da4643c56d8199b63c93f622ad0
Fernando Menezes <fmrodrigues92@yahoo.com>
2022-09-28 17:57:16 -0300

    update routing

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,7 +12,7 @@
 
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Aloware</a>
+              <a class="navbar-brand" href="/">Aloware</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -20,9 +20,6 @@
                 <ul class="navbar-nav">
                   <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="/">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('blog') }}">Blog</a>
                   </li>
                 </ul>
               </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('/', [HomeController::class, 'index'])->name('home');
