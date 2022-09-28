3815d8dc197c955cc3ec203d3215297f9865f956
Fernando Menezes <fmrodrigues92@yahoo.com>
2022-09-28 17:52:50 -0300

    layout start

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+
+            <h1>Home Page</h1>
+            
+        </div>
+    </div>
+
+@endsection
